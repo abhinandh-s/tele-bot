47979118098a1faa50cd8f706ba6d4232b28fd60
Abhinandh S <abhinandhsuby@proton.me>
2025-09-07 23:01:45 +0530

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -68,7 +68,7 @@ async fn process_repos(token: &str, chat_id: &str, repos: Vec<&str>) -> anyhow::
 
         println!("New release found for {}: {}", repo, release.tag_name);
 
-        crate::download_assets_concurrent(&repo, &release).await?;
+        download_assets_concurrent(&repo, &release).await?;
 
         let mut message = format!("🚀 New Release from *{}*: *{}*\n\n", repo, release.tag_name);
         let mut sent_text = false;
