6c51782c29eb41fd0b711d3d20bb35217b219391
Abhinandh S <abhinandhsuby@proton.me>
2025-09-08 02:39:16 +0530

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -217,11 +217,11 @@ async fn download_assets_concurrent(repo: &str, release: &Release) -> anyhow::Re
         t.await??;
     }
 
-// Write the file list
-    let mut list_file = tokio::fs::File::create("assets/list.txt").await?;
+    // Write the file list
+    let mut list_file = fs::File::create("assets/list.txt")?;
     for path in &file_paths {
-        list_file.write_all(path.as_bytes()).await?;
-        list_file.write_all(b"\n").await?;
+        list_file.write_all(path.as_bytes())?;
+        list_file.write_all(b"\n")?;
     }
 
     Ok(file_paths)
